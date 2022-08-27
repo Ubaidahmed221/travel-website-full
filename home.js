18a7fc4464bb0e6486899dc6862b471bcc322074
ubaid-12 <ubaidahmed221@gmail.com>
2022-08-27 08:43:22 -0700

    travel page website

---
+++ home.js
@@ -0,0 +1,48 @@
+let searchbtn = document.querySelector('#search-btn')
+let searchbar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+let vediobtn = document.querySelectorAll('.vid-btn');
+
+
+window.onscroll =() => {
+    searchbtn.classList.remove('fa-times');
+    searchbar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+
+searchbtn.addEventListener('click', () => {
+    searchbtn.classList.toggle('fa-times');
+    searchbar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () => {
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () => {
+    loginForm.classList.remove('active');
+});
+
+vediobtn.forEach(btn => {
+    btn.addEventListener('click',()=>{
+        document.querySelector('.controls .active').classList.remove('active')
+        btn.classList.add('active')
+        let src =btn.getAttribute('data-src');
+        document.querySelector('#vedio-slider').src = src;
+    });
+});
+
+
